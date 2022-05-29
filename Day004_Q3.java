6dd479415d76dcad246f2615d4cd45d4b26a83e9
PyrokineticRaj <rajesh.kumar.das.tech@gmail.com>
2022-05-29 13:02:15 +0530

    d4q3

--- Day004_Q3.java
+++ Day004_Q3.java
@@ -1,5 +1,25 @@
+import java.util.*;
 public class Day004_Q3 {
     public static void main(String[] args) {
-        
+        Day004_Q3_Instrument iarr[] = new Day004_Q3_Instrument[10];
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            int randomNum = rand.nextInt(3 - 0) + 1;
+            if (randomNum == 1)
+                iarr[i] = new Piano_D4Q3();
+            else if (randomNum == 2)
+                iarr[i] = new Flute_D4Q3();
+            else if (randomNum == 3)
+                iarr[i] = new Guitar_D4Q3();
+            iarr[i].Play();
+        }
+        for (int i = 0; i < 10; i++) {
+            if (iarr[i] instanceof Piano_D4Q3)
+                System.out.println("Piano is stored at index " + i);
+            else if (iarr[i] instanceof Flute_D4Q3)
+                System.out.println("Flute is stored at index " + i);
+            else if (iarr[i] instanceof Guitar_D4Q3)
+                System.out.println("Guitar is stored at index " + i);
+        }
     }
 }
--- Day004_Q3_Instrument.java
+++ Day004_Q3_Instrument.java
@@ -1,3 +1,20 @@
 abstract class Day004_Q3_Instrument {
+    abstract void Play();
     
+}
+
+ class Piano_D4Q3 extends Day004_Q3_Instrument{
+    void Play() {
+        System.out.println("Piano is playing tan tan tan tan");
+    }
+}
+ class Flute_D4Q3 extends Day004_Q3_Instrument{
+    void Play() {
+        System.out.println("Flute is playing toot toot toot toot");
+    }
+}
+ class Guitar_D4Q3 extends Day004_Q3_Instrument{
+    void Play() {
+        System.out.println("Guitar is playing tin tin tin ");
+    }
 }
