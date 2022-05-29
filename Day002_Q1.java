9a76a20d53a1777f99bd79b9d433fc7bdc9144ce
PyrokineticRaj <rajesh.kumar.das.tech@gmail.com>
2022-05-29 12:17:25 +0530

    modified d2q1

--- Day002_Q1.java
+++ Day002_Q1.java
@@ -1,21 +1,24 @@
 public class Day002_Q1 {
-	
+
 	public static void main(String[] args) {
-		Student_D2Q1 s1 = new Student_D2Q1(001, "Rajesh", 23456L, "Java", 20000);
-		Student_D2Q1 s2 = new Student_D2Q1(002, "Jarvis", 234322L, "Java", 20000);
-		Student_D2Q1 s3 = new Student_D2Q1(003, "Abdul", 267576L, "Java", 20000);
-		Student_D2Q1 s4 = new Student_D2Q1(004, "Ethan", 233256L, "Java", 20000);
-		Student_D2Q1 s5 = new Student_D2Q1(005, "Nithin", 986546L, "Java", 20000);
-		System.out.println("\nDetails of Student 1 is:");
-		s1.Display();
-		System.out.println("\nDetails of Student 2 is:");
-		s2.Display();
-		System.out.println("\nDetails of Student 3 is:");
-		s3.Display();
-		System.out.println("\nDetails of Student 4 is:");
-		s4.Display();
-		System.out.println("\nDetails of Student 5 is:");
-		s5.Display();
+		Student_D2Q1 sarr[] = new Student_D2Q1[5];
+		sarr[0] = new Student_D2Q1(001, "Rajesh", 23456L, "Java", 20000);
+		sarr[1] = new Student_D2Q1(002, "Jarvis", 234322L, "Java", 20000);
+		sarr[2] = new Student_D2Q1(003, "Abdul", 267576L, "Java", 20000);
+		sarr[3] = new Student_D2Q1(004, "Ethan", 233256L, "Java", 20000);
+		sarr[4] = new Student_D2Q1(005, "Nithin", 986546L, "Java", 20000);
+
+		for (int i = 0; i < sarr.length; i++) {
+			System.out.println("\nDetails of Student " + (i + 1) + " is: ");
+			sarr[i].Display();
+		}
+
+		int totalfees = 0;
+		for (int i = 0; i < sarr.length; i++) {
+			totalfees = totalfees + sarr[i].fees;
+		}
+		System.out.println("\nThe total fees of all the student is:"+ totalfees);
+
 	}
 }
 
@@ -36,8 +39,8 @@ class Student_D2Q1 {
 
 	void Display() {
 		System.out.println("Student ID: " + studentid);
-		System.out.println("Name: "+name);
-		System.out.println("Contact Number: "+ contactnumber);
+		System.out.println("Name: " + name);
+		System.out.println("Contact Number: " + contactnumber);
 		System.out.println("Coures: " + course);
 		System.out.println("Fees: " + fees);
 	}
