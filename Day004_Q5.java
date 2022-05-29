4d511f622fcd680cdff28998a703afaebd388f45
PyrokineticRaj <rajesh.kumar.das.tech@gmail.com>
2022-05-29 17:30:28 +0530

    d4q5 done

---
+++ Day004_Q5.java
@@ -0,0 +1,15 @@
+import Day004_Q5_Test.*;
+
+public class Day004_Q5 {
+    public static void main(String[] args) {
+        Foundation f = new Foundation();
+        // To check individual access modifiers, just comment other invoked variables.
+        // System.out.println(f.var1);
+        // System.out.println(f.var2);
+        // System.out.println(f.var3);
+        System.out.println(f.var4);
+
+    }
+    
+    
+}
