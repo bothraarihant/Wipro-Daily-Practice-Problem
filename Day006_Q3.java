a9c8c007bdfb96a6d9e9c3cb3266796235fdada5
PyrokineticRaj <rajesh.kumar.das.tech@gmail.com>
2022-05-30 18:26:54 +0530

    d6q3 done

---
+++ Day006_Q3.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class Day006_Q3 {
+    public static void main(String[] args) {
+        List<Object> list = new MyArrayList<>();
+        list.add(1);
+        list.add("0");
+        list.add(3.14);
+        System.out.println(list);
+    }
+}
+
+class MyArrayList<E> extends ArrayList<E>{
+    public boolean add(E e) {
+        if (e instanceof Integer || e instanceof Float || e instanceof Double) {
+            super.add(e);
+            return true;
+        }
+        else {
+            throw new ClassCastException("Only numbers are allowed");
+            // return false; // if we return false, it will just through without throwing an exception
+        }
+    }
+}
