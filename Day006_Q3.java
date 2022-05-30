5b1e3fc49fec0e89070b356ca3fc3402cbef7908
PyrokineticRaj <rajesh.kumar.das.tech@gmail.com>
2022-05-30 18:27:09 +0530

    d6q3 done

--- Day006_Q3.java
+++ Day006_Q3.java
@@ -11,15 +11,15 @@ public class Day006_Q3 {
     }
 }
 
-class MyArrayList<E> extends ArrayList<E>{
+class MyArrayList<E> extends ArrayList<E> {
     public boolean add(E e) {
         if (e instanceof Integer || e instanceof Float || e instanceof Double) {
             super.add(e);
             return true;
-        }
-        else {
+        } else {
             throw new ClassCastException("Only numbers are allowed");
-            // return false; // if we return false, it will just through without throwing an exception
+            // return false; // if we return false, it will just through without throwing an
+            // exception
         }
     }
 }
